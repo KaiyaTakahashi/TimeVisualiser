d3e85e3b7b29397e2e504a8182d36e12b6dee5b5
Kaiya Takahashi <yaikashi@icloud.com>
2022-07-03 17:49:34 -0700

    Initial Commit

---
+++ TimeVisualiser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeVisualiser
+//
+//  Created by Kaiya Takahashi on 2022-07-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
