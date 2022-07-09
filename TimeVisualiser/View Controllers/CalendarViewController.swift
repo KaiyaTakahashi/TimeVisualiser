80ecaa2942338032bd0ee5e33d2ea00a168c6d17
Kaiya Takahashi <yaikashi@icloud.com>
2022-07-08 23:10:04 -0700

    feat: add dataSource and delegate code in CalendarViewController

--- TimeVisualiser/View Controllers/CalendarViewController.swift
+++ TimeVisualiser/View Controllers/CalendarViewController.swift
@@ -7,16 +7,39 @@
 
 import UIKit
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet var monthYearLabel: UILabel!
     @IBOutlet var previousMonthBtn: UIButton!
     @IBOutlet var nextMonthBtn: UIButton!
     @IBOutlet var calendarCollectionView: UICollectionView!
+    
+    var selectedDate = Date()
+    var totalSquares = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    func setupCell() {
+        let width = (calendarCollectionView.frame.size.width - 2) / 8
+        let height = (calendarCollectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = calendarCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = calendarCollectionView.dequeueReusableCell(withReuseIdentifier: "CalCell", for: indexPath) as! CalendarCollectionViewCell
+        cell.dayOfMonth.text = totalSquares[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return totalSquares.count
+    }
+
 
     @IBAction func previousMonthBtnTapped(_ sender: UIButton) {
     }
